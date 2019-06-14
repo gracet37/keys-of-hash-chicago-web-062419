3c1e274b7387df571048ba97d07a30d5983f328e
Grace Tan <gracetan37@gmail.com>
2019-06-14 16:13:55 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,6 @@
 class Hash
   def keys_of(*arguments)
     # code goes here
-map {|key,value| arguments.include?(value) ? key : nil}.compact #this will return an array of x,nil,y,nil etc. Compact will remove all nils
-end 
+map {|key,value| arguments.include?(value) ? key : nil}.compact #this will return an array of x,nil,y,nil etc. Compact will remove all nils. It takes a copy of self and removes nils.
+ end 
 end 
