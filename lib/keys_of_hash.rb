209ebf4951ee46a5ba756bd65f7a0f3014e36db4
Grace Tan <gracetan37@gmail.com>
2019-06-14 16:08:38 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,10 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
-  end
-end
+array = []
+if hash.has_value?(arguments)
+  array << hash.key(value)
+end 
+return array
+end 
+end 
